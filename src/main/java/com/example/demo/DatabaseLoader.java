e2e5f2d886d41b6de23cc1cb709c12077ff569f2
reynaldo garcia tito <rey_naldo2311@hotmail.com>
2023-08-17 16:12:38 -0500

    2 tablas

--- src/main/java/com/example/demo/DatabaseLoader.java
+++ src/main/java/com/example/demo/DatabaseLoader.java
@@ -4,19 +4,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-@Component 
-public class DatabaseLoader implements CommandLineRunner { 
+@Component
+public class DatabaseLoader implements CommandLineRunner {
 
-	private final InstrumentoRepository repository;
+	private final InstrumentoRepository repositoryInstrumento;
+	private final MusicoRepository repositoryMusico;
 
-	@Autowired 
-	public DatabaseLoader(InstrumentoRepository repository) {
-		this.repository = repository;
+	@Autowired
+	public DatabaseLoader(InstrumentoRepository repositoryInstrumento, MusicoRepository repositoryMusico) {
+		this.repositoryInstrumento = repositoryInstrumento;
+		this.repositoryMusico = repositoryMusico;
+
 	}
 
 	@Override
-	public void run(String... strings) throws Exception { 
-		this.repository.save(new Instrumento("Guitarra", "Instrumento de cuerda", "De madera, con una caja de resonancia"));
-		this.repository.save(new Instrumento("Ukelele", "Instrumento de viento", "De madera, con una caja de resonancia potente"));
+	public void run(String... strings) throws Exception {
+		this.repositoryInstrumento
+				.save(new Instrumento("Guitarra", "Instrumento de cuerda", "De madera, con una caja de resonancia"));
+		this.repositoryInstrumento.save(
+				new Instrumento("Ukelele", "Instrumento de viento", "De madera, con una caja de resonancia potente"));
+		this.repositoryMusico.save(
+				new Musico("Jimi Hendrix"));
+		this.repositoryMusico.save(
+				new Musico("Eric Clapton"));
 	}
 }
--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,31 +1,40 @@
 const React = require('react');
-const ReactDOM = require('react-dom'); 
-const client = require('./client'); 
+const ReactDOM = require('react-dom');
+const client = require('./client');
 
-class App extends React.Component { 
+class App extends React.Component {
 
 	constructor(props) {
 		super(props);
-		this.state = {instrumentos: []};
+		this.state = { instrumentos: [], musicos: []};
 	}
 
-	componentDidMount() { 
-		client({method: 'GET', path: '/api/instrumentos'}).done(response => {
-			this.setState({instrumentos: response.entity._embedded.instrumentos});
+	componentDidMount() {
+		client({ method: 'GET', path: '/api/instrumentos' }).done(response => {
+			this.setState({ instrumentos: response.entity._embedded.instrumentos });
+		});
+		client({ method: 'GET', path: '/api/musicos' }).done(response => {
+			this.setState({ musicos: response.entity._embedded.musicos });
 		});
 	}
 
-	render() { 
+	render() {
 		return (
-			<InstrumentoList instrumentos={this.state.instrumentos}/>
+			<>
+				<h2>Instrumentos</h2>
+				<InstrumentoList instrumentos={this.state.instrumentos} />
+				<hr />
+				<h2>Musicos</h2>
+				<MusicoList musicos={this.state.musicos} />
+			</>
 		)
 	}
 }
 
-class InstrumentoList extends React.Component{
+class InstrumentoList extends React.Component {
 	render() {
 		const instrumentos = this.props.instrumentos.map(instrumento =>
-			<Instrumento key={instrumento._links.self.href} instrumento={instrumento}/>
+			<Instrumento key={instrumento._links.self.href} instrumento={instrumento} />
 		);
 		return (
 			<table border="1">
@@ -42,13 +51,41 @@ class InstrumentoList extends React.Component{
 	}
 }
 
-class Instrumento extends React.Component{
+class MusicoList extends React.Component {
+	render() {
+		const musicos = this.props.musicos.map(musico =>
+			<Musico key={musico._links.self.href} musico={musico} />
+		);
+		return (
+			<table border="1">
+				<tbody>
+					<tr>
+						<th>Nombre</th>
+					</tr>
+					{musicos}
+				</tbody>
+			</table>
+		)
+	}
+}
+
+class Instrumento extends React.Component {
 	render() {
 		return (
 			<tr>
 				<td>{this.props.instrumento.nombre}</td>
 				<td>{this.props.instrumento.categoria}</td>
 				<td>{this.props.instrumento.descripcion}</td>
+			</tr>
+		)
+	}
+}
+
+class Musico extends React.Component {
+	render() {
+		return (
+			<tr>
+				<td>{this.props.musico.nombre}</td>
 			</tr>
 		)
 	}
